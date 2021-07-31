464bb996eeef72b701ee57e76dbfd12a4a79e884
Simon Siefke <simon.siefke@gmail.com>
2021-07-31 11:55:45 +0200

    update demo

--- demo/server.js
+++ demo/server.js
@@ -1,64 +1,67 @@
-import { forkPtyAndExecvp } from 'fork-pty'
-import fs from 'fs'
-import http from 'http'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import WebSocket from 'ws'
+import { forkPtyAndExecvp } from "fork-pty";
+import fs from "fs";
+import http from "http";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import WebSocket from "ws";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const Terminal = {
   terminals: Object.create(null),
   create(socket, id) {
-    const { ptySocket } = forkPtyAndExecvp('bash', ['bash', '-i'])
-    this.terminals[id] = ptySocket
+    const { ptySocket } = forkPtyAndExecvp("bash", ["bash", "-i"]);
+    this.terminals[id] = ptySocket;
     const handleData = (data) => {
       socket.send(
-        JSON.stringify([/* terminalWrite */ 2, /* id */ id, /* data */ data]),
-      )
-    }
-    ptySocket.on('data', handleData)
+        JSON.stringify([/* terminalWrite */ 2, /* id */ id, /* data */ data])
+      );
+    };
+    ptySocket.on("data", handleData);
   },
   write(socket, id, data) {
-    this.terminals[id].write(data)
+    this.terminals[id].write(data);
   },
   dispose(socket, id) {
-    this.terminals[id].dispose()
-    delete this.terminals[id]
+    this.terminals[id].dispose();
+    delete this.terminals[id];
   },
-}
+  resize(socket, id, columns, rows) {
+    // TODO
+  },
+};
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    fs.createReadStream(`${__dirname}/index.html`).pipe(res)
+  if (req.method === "GET" && req.url === "/") {
+    fs.createReadStream(`${__dirname}/index.html`).pipe(res);
   } else {
-    res.statusCode = 404
-    res.end('not found')
+    res.statusCode = 404;
+    res.end("not found");
   }
-})
+});
 
-const wss = new WebSocket.Server({ server })
+const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (socket) => {
-  socket.on('message', (message) => {
-    if (typeof message !== 'string') {
-      return
+wss.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    if (typeof message !== "string") {
+      return;
     }
-    const [commandId, ...args] = JSON.parse(message)
+    const [commandId, ...args] = JSON.parse(message);
     switch (commandId) {
       case 101:
-        Terminal.create(socket, ...args)
-        break
+        Terminal.create(socket, ...args);
+        break;
       case 102:
-        Terminal.write(socket, ...args)
-        break
+        Terminal.write(socket, ...args);
+        break;
       case 103:
-        Terminal.dispose(socket, ...args)
-        break
+        Terminal.dispose(socket, ...args);
+        break;
       default:
-        break
+        break;
     }
-  })
-})
+  });
+});
 
-server.listen(3000, () => console.log(`listening on http://localhost:3000`))
+server.listen(3000, () => console.log(`listening on http://localhost:3000`));
