c1b2397ac7763820a6761dcb57d79e04fd83fc62
Simon Siefke <simon.siefke@gmail.com>
2023-07-28 14:39:02 +0200

    refactor: move parser state enum into separate file

---
+++ src/parts/ParserState/ParserState.js
@@ -0,0 +1,16 @@
+export const TopLevelContent = 1;
+export const Escaped = 2;
+export const Csi = 3;
+export const AfterEscape3 = 4;
+export const Charset = 5;
+export const AfterEscape3AfterSemicolon = 6;
+export const Osc = 8;
+export const Dcs = 9;
+export const AfterQuestionMark = 10;
+export const AfterQuestionMark2 = 11;
+export const AfterExclamationMark = 12;
+export const AfterExclamationMark2 = 13;
+export const AfterSpace = 14;
+export const AfterSpace2 = 15;
+export const Osc2 = 16;
+export const Osc3 = 17;
