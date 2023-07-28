a99d0135f9768a334493294736ed2f9edcf5bd1a
Simon Siefke <simon.siefke@gmail.com>
2023-07-28 20:25:40 +0200

    add build package

---
+++ packages/build/src/parts/ComputeNodeModulesCacheKey/ComputeNodeModulesCacheKey.js
@@ -0,0 +1,47 @@
+import { createHash } from "node:crypto";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import * as Root from "../Root/Root.js";
+
+const locations = [
+  "lerna.json",
+  "package-lock.json",
+  ".github/workflows/ci.yml",
+  ".github/workflows/pr.yml",
+  ".github/workflows/release.yml",
+  "scripts/computeNodeModulesCacheKey.js",
+];
+
+const getAbsolutePath = (relativePath) => {
+  return join(Root.root, relativePath);
+};
+
+const getContent = (absolutePath) => {
+  return readFile(absolutePath, "utf8");
+};
+
+export const computeHash = (contents) => {
+  const hash = createHash("sha1");
+  if (Array.isArray(contents)) {
+    for (const content of contents) {
+      hash.update(content);
+    }
+  } else if (typeof contents === "string") {
+    hash.update(contents);
+  }
+  return hash.digest("hex");
+};
+
+const computeCacheKey = async (locations) => {
+  const absolutePaths = locations.map(getAbsolutePath);
+  const contents = await Promise.all(absolutePaths.map(getContent));
+  const hash = computeHash(contents);
+  return hash;
+};
+
+const main = async () => {
+  const hash = await computeCacheKey(locations);
+  process.stdout.write(hash);
+};
+
+main();
