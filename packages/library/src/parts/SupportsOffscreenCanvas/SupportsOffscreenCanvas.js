aa7fc8ff581d25ad1cf1c1fff72497a268b6448b
Simon Siefke <simon.siefke@gmail.com>
2023-12-15 23:45:45 +0100

    feature: assume offscreencanvas is supported, avoid needing to create… (#53)

--- packages/library/src/parts/SupportsOffscreenCanvas/SupportsOffscreenCanvas.js
+++ packages/library/src/parts/SupportsOffscreenCanvas/SupportsOffscreenCanvas.js
@@ -1,11 +1,1 @@
-import { CHAR_HEIGHT, CHAR_WIDTH } from "../CharSize/CharSize.js";
-
-export const supportsOffscreenCanvas = (() => {
-  try {
-    const canvas = new OffscreenCanvas(CHAR_WIDTH, CHAR_HEIGHT);
-    canvas.getContext("2d");
-    return true;
-  } catch {
-    return false;
-  }
-})();
+export const supportsOffscreenCanvas = true;
