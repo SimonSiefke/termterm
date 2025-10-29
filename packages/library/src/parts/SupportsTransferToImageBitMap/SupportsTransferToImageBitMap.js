01b00a386f11114cb0ecf8920b29d39691feb8f6
Simon Siefke <simon.siefke@gmail.com>
2025-10-29 17:34:40 +0100

    feature: update supportsTransferToImageBitmap function to not run at start to reduce startup time

--- packages/library/src/parts/SupportsTransferToImageBitMap/SupportsTransferToImageBitMap.js
+++ packages/library/src/parts/SupportsTransferToImageBitMap/SupportsTransferToImageBitMap.js
@@ -1,7 +1,7 @@
 import { CHAR_WIDTH, CHAR_HEIGHT } from "../CharSize/CharSize.js";
 import { supportsOffscreenCanvas } from "../SupportsOffscreenCanvas/SupportsOffscreenCanvas.js";
 
-export const supportsTransferToImageBitMap = (() => {
+const supportsTransferToImageBitMapInternal = () => {
   try {
     supportsOffscreenCanvas
       ? (() => {
@@ -13,10 +13,20 @@ export const supportsTransferToImageBitMap = (() => {
           const canvas = document.createElement("canvas");
           canvas.width = CHAR_WIDTH;
           canvas.height = CHAR_HEIGHT;
+          // @ts-ignore
           canvas.transferToImageBitmap();
         })();
     return true;
   } catch {
     return false;
   }
-})();
+};
+
+let cachedValue = undefined;
+
+export const supportsTransferToImageBitMap = () => {
+  if (cachedValue === undefined) {
+    cachedValue = supportsTransferToImageBitMapInternal();
+  }
+  return cachedValue;
+};
